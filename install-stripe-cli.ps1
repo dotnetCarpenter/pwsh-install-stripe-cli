5c159b7ebeb0df271ebf71fab93f2b5789678551
dotnetCarpenter <jon.ronnenberg@gmail.com>
2025-07-28 09:48:09 +0200

    output assets from the latest release

--- install-stripe-cli.ps1
+++ install-stripe-cli.ps1
@@ -1,13 +1,20 @@
 #!/usr/bin/env pwsh
 
-$GITHUB_RELEASE_JSON_URL = "https://api.github.com/repos/stripe/stripe-cli/releases/latest"
 $GITHUB_HEADERS = ${
-	"Accept": "application/vnd.github+json"
+	"Accept": "application/vnd.github+json",
+	"X-GitHub-Api-Version": "2022-11-28"
 }
+$GITHUB_RELEASE_URL = "https://api.github.com/repos/stripe/stripe-cli/releases/latest"
+# $GITHUB_RELEASE_ASSETS_URL = "https://api.github.com/repos/stripe/stripe-cli/releases/assets"#/ASSET_ID
 
-$GITHUB_RESPONSE = Invoke-WebRequest -Uri $GITHUB_RELEASE_JSON_URL -Headers $GITHUB_HEADERS
+$GITHUB_STRIPE_ASSETS = Invoke-WebRequest -Uri $GITHUB_RELEASE_URL -Headers $GITHUB_HEADERS
 	| Select-Object -ExpandProperty "Content" # will return a string
 	# | Select-Object -Property "Content" # will return an object
 	| ConvertFrom-Json # input is a string
+	| Select-Object -ExpandProperty "assets"
 
-Write-Output $GITHUB_RESPONSE
+Write-Output $GITHUB_STRIPE_ASSETS
+# Write-Output "Release ID: $GITHUB_RELEASE_ID"
+
+# Invoke-WebRequest -Uri "$GITHUB_RELEASE_ASSETS_URL/$GITHUB_RELEASE_ID" -Headers $GITHUB_HEADERS
+
